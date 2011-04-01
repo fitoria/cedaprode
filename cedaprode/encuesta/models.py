20da08564d24b956a4e90cb378954ba7e4ece323
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-04-01 17:04:40 -0600

    modelos básicos

--- cedaprode/encuesta/models.py
+++ cedaprode/encuesta/models.py
@@ -1,3 +1,30 @@
+# -*- coding: UTF-8 -*-
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    '''Categoria principal'''
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Categorías'
+
+    def __unicode__(self):
+        return self.titulo
+
+class Pregunta(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto_explicativo = models.TextField()
+
+    def __unicode__(self):
+        return self.titulo
+
+class Opcion(models.Model):
+    titulo = models.CharField(max_length=200)
+    puntaje = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name_plural = 'Opciones'
+
+    def __unicode__(self):
+        return '%s= %s' % (self.puntaje, self.titulo)

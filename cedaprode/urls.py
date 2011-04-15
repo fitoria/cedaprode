66e23455cb5951d2008a605136cda4f96c8e9245
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-04-15 13:55:27 -0600

    agregando logout

--- cedaprode/urls.py
+++ cedaprode/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='user-login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='user-logout'),
     url(r'^', include('cedaprode.encuesta.urls')),
 )
 

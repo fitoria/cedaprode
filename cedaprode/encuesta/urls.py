54eddce2cdb7aa108abadcf57910a73e3045ef66
Byron <byroncorrales@gmail.com>
2011-04-15 13:51:12 -0600

    login form

--- cedaprode/encuesta/urls.py
+++ cedaprode/encuesta/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('encuesta.views',
             url(r'^llenar-encuesta/(?P<encuesta_id>\d+)/$', 'llenar_encuesta', name='llenar-encuesta'),
             url(r'^crear-encuesta/$', 'crear_encuesta', name='crear-encuesta'),
             url(r'^crear-organizacion/$', 'crear_organizacion', name='crear-organizacion'),
+            url(r'^inicio/$', 'inicio'),
             )

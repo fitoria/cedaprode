7a89470a45b93d51cc8873f025290f665b668c40
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-05-04 13:37:11 -0600

    reparando stepy

--- cedaprode/encuesta/templatetags/clasificador.py
+++ cedaprode/encuesta/templatetags/clasificador.py
@@ -13,3 +13,24 @@ def clasificador(value):
        return base_url + 'azul.png'
     else:
         raise Exception('la shit no es correcta')
+
+@register.filter
+def categorizador(value):
+    '''Value es el formset'''
+    resultados = []
+    fila = {'categoria': None, 'forms': []}
+    for index, form in enumerate(value):
+        if fila['categoria'] == None:
+            fila['categoria'] = form.instance.pregunta.categoria
+            fila['forms'].append(form)
+        elif fila['categoria'] != form.instance.pregunta.categoria:
+            resultados.append(dict.copy(fila))
+            fila['categoria'] = form.instance.pregunta.categoria
+            fila['forms'] = [form]
+        elif fila['categoria'] == form.instance.pregunta.categoria:
+            fila['forms'].append(form)
+            #para que aparezca el fucking paso nueve
+            if index == len(value) -1 :
+                resultados.append(dict.copy(fila))
+
+    return resultados

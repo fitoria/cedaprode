f3160cf924f69f656afa892807e67fc4cd76ae0f
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-05-06 10:29:14 -0600

    reparando grafico consolidado

--- cedaprode/encuesta/utils.py
+++ cedaprode/encuesta/utils.py
@@ -4,18 +4,24 @@ from pygooglechart import RadarChart as Grafo
 def generar_grafo(queryset, titulo):
     '''Genera un grafo a partir de un queryset o lista de respuestas'''
     data = []
-    #legends = []
+    legends = []
     labels = []
     for respuesta in queryset:
         try:
             data.append(int(respuesta.respuesta.puntaje))
-            labels.append(respuesta.pregunta.titulo[:10])
+            labels.append(respuesta.pregunta.titulo[:2])
+            legends.append(respuesta.pregunta.titulo[:50])
         except:
             pass
 
-    chart = Grafo(400, 350, auto_scale=True, x_range=(0,5), y_range=(0,5))
+    if len(data) > 2:
+        data.append(data[0])
+
+    chart = Grafo(600, 350, auto_scale=True, x_range=(0,5), y_range=(0,5))
     chart.add_data(data)
     chart.set_title(titulo)
+    chart.set_legend(legends)
+    chart.set_legend_position('r')
     chart.set_axis_labels('x', labels)
     chart.set_axis_range('y', 0, 5)
     try:
@@ -24,11 +30,14 @@ def generar_grafo(queryset, titulo):
         return '/files/imagen/grafoerror.jpg'
 
 def generar_grafro_general(titulo, filas, ejes):
-    chart = Grafo(400, 350, auto_scale=True, x_range=(0,5), y_range=(0,5))
+    chart = Grafo(600, 350, auto_scale=True, x_range=(0,100), y_range=(0,100))
+    filas.append(filas[0])
     chart.add_data(filas)
     chart.set_title(titulo)
-    chart.set_axis_labels('x', ejes)
-    chart.set_axis_range('y', 0, 5)
+    chart.set_axis_labels('x', range(1, len(ejes)+ 1))
+    legends = [(str(i+1) + '-' + j) for i, j in enumerate(ejes)] 
+    chart.set_legend(legends)
+    chart.set_axis_range('y', 0, 30)
     try:
         return chart.get_url()
     except:

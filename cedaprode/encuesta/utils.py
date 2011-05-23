6cc50b1ba06a9b995e914be2b979ce34f7865079
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2011-05-23 16:04:48 -0600

    prueba

--- cedaprode/encuesta/utils.py
+++ cedaprode/encuesta/utils.py
@@ -35,20 +35,9 @@ def generar_grafro_general(titulo, filas, ejes):
                 radar=generar_grafo_radar_norm(titulo, filas, ejes))
 
 def generar_grafo_barra(titulo, filas, ejes):
-    chart = Barra(620, 340, auto_scale=True, x_range=(0,30), y_range=(0,30))
-    chart.set_colours(['1737C2','5C6694'])
-    chart.add_data([f[0] for f in filas])
-    chart.add_data([f[1] for f in filas])
-    chart.set_title(titulo)
+    data = [[f[0] for f in filas], [f[1] - f[0] for f in filas]]
     legends = ['%s (%s/%s)' % (i, j[0], j[1]) for i, j in zip(ejes, filas)] 
-    chart.set_axis_labels('y', legends)
-    chart.set_legend(['Puntaje alcanzado', 'Total posible'])
-    chart.set_axis_range('x', 0, 30)
-    chart.set_line_style(0, 2)
-    try:
-        return chart.get_url()
-    except:
-        return '/files/imagen/grafoerror.jpg'
+    return dict(data=data, legends=legends)
 
 def generar_grafo_radar_norm(titulo, filas, ejes):
     chart = Grafo(600, 350, auto_scale=True, x_range=(0,100), y_range=(0,100))
